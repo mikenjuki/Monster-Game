86b101c2a97171f120a83b178abb8ffc780e722e
Michael Njuki <mikenjuki@gmail.com>
2022-10-24 10:01:33 +0300

    Setup Monster Game

---
+++ assets/scripts/app.js
@@ -0,0 +1,25 @@
+const ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 14;
+
+let userInputLife = 100;
+let currentMonsterHealth = userInputLife;
+let currentPlayerHealth = userInputLife;
+
+
+
+adjustHealthBars(userInputLife);
+
+attackBtn.addEventListener("click", attackHandler);
+
+function attackHandler() {
+    const damage = dealMonsterDamage(ATTACK_VALUE);
+    currentMonsterHealth -= damage;
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+    currentPlayerHealth -= playerDamage;
+
+    if (currentMonsterHealth <= 0) {
+        alert("You Won!");
+    } else if (currentPlayerHealth <=0) {
+        alert("You almost got it! Try Again.");
+    }
+}
