f3c5f61826f8ae81fe7efa7f1005fe6a28863330
Michael Njuki <mikenjuki@gmail.com>
2022-10-25 08:54:49 +0300

    Shed excess code

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,40 +6,40 @@ let userInputLife = 100;
 let currentMonsterHealth = userInputLife;
 let currentPlayerHealth = userInputLife;
 
-
-
 adjustHealthBars(userInputLife);
 
-attackBtn.addEventListener("click", attackHandler);
+
+function attackMonster(mode) {
+    let blowDamage;
+    if (mode === "ATTACK") {
+        blowDamage = ATTACK_VALUE;
+    } else if (mode === "STRONG_ATTACK") {
+        blowDamage = STRONG_ATTACK_VALUE;
+    } else {
+        alert("You didn't attack!")
+    }
+    const damage = dealMonsterDamage(blowDamage);
+    currentMonsterHealth -= damage;
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+    currentPlayerHealth -= playerDamage;
+
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+        alert("You Won!");
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+        alert("You almost got it! Try Again.");
+    } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+        alert("It's a draw!");
+    }
+}
 
 function attackHandler() {
-    const damage = dealMonsterDamage(ATTACK_VALUE);
-    currentMonsterHealth -= damage;
-    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
-    currentPlayerHealth -= playerDamage;
-
-    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
-        alert("You Won!");
-    } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
-        alert("You almost got it! Try Again.");
-    } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
-        alert("It's a draw!");
-    }
+    attackMonster("ATTACK");
 }
 
-strongAttackBtn.addEventListener("click", strongAttackHandler);
 
 function strongAttackHandler() {
-    const damage = dealMonsterDamage(STRONG_ATTACK_VALUE);
-    currentMonsterHealth -= damage;
-    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
-    currentPlayerHealth -= playerDamage;
-
-    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
-        alert("You Won!");
-    } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
-        alert("You almost got it! Try Again.");
-    } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
-        alert("It's a draw!");
-    }
+    attackMonster("STRONG_ATTACK");
 }
+
+attackBtn.addEventListener("click", attackHandler);
+strongAttackBtn.addEventListener("click", strongAttackHandler);
